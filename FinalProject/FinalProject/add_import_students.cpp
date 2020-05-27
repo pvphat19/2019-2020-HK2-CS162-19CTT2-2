8c3c2db16cebdff988355e7632dcd1421db17004
pvphat19 <64125852+pvphat19@users.noreply.github.com>
2020-05-27 14:26:10 +0700

    hoan thien ham import student

--- FinalProject/FinalProject/add_import_students.cpp
+++ FinalProject/FinalProject/add_import_students.cpp
@@ -249,183 +249,216 @@ void manually_add_student_to_class()//chua kt ngay thang nam
 	deleteStudentList(pAllStudent);
 }
 
-void import_class()//can nang cap doan check class ban dau
+void import_class()
 {
-	cout << "Please enter the name of the class: ";
+	int x;
+	int y;
+	system("cls");
+	x = 10; y = 5;
+	goToXY(x, y++); cout << "Import a class from csv file.";
+	goToXY(x, y++); cout << "Please enter the name of the class: ";
+	//nhap ten class
 	string classname;
-	getline(cin, classname);
-	//check the class is existed or not
-	char* temp = new char[classname.size() + 1];
-	for (int i = 0; i < classname.size(); ++i)
-		temp[i] = classname[i];
-	temp[classname.size()] = '\0';
-	ifstream in;
-	in.open("class.txt");
-	int n;
-	in >> n;
-	in.ignore();
-	char** cla;
-	cla = new char* [n];
-	bool t = true;
-	for (int i = 0; i < n; ++i)
-	{
-		char tmp[100];
-		in.get(tmp, 100, '\n');
-		in.get();
-		cla[i] = new char[strlen(tmp) + 1];
-		strcpy(cla[i], tmp);
-		if (strcmp(temp, cla[i]) == 0)
-			t = false;
-	}
-	for (int i = 0; i < n; ++i)
-		delete[]cla[i];
-	delete[]cla;
-	delete[] temp;
-	in.close();
-	if (t == false)
-	{
-		cout << "The class is existed." << endl;
-		return;
-	}
-	else
-	{
-		//chua cap nhat Class.txt vi con dem so hoc sinh o sau
-		cout << "This is a new class." << endl;
-	}
-	//end of checking
-	//import
-	cout << "Please enter the link of csv file:";
-	string csv;
-	getline(cin, csv);
-	in.open(csv);
-	if (!in)
-	{
-		cout << "Error when opening the csv file." << endl;
-		return;
-	}
-	string filename = classname + ".txt";
-	ofstream out;
-	out.open(filename,std::ios::_Noreplace);
-	if (!out)
-	{
-		cout << "Error in creating the file." << endl;
-		return;
-	}
-		int success = 0;
-		Student* pStudent = nullptr;
-		Student* cur = pStudent;
-		int dem = 0;
-		int numstudent1 = 0;
-		Student* pStudent1 = nullptr;
-		loadStudent(numstudent1, pStudent1, "Student");
-		in.ignore(1000, '\n');
-		while (!in.eof())
+	int choice = 1;
+	int numClass = 0;
+	Class* pClass = nullptr;
+	loadClass(numClass, pClass);
+	while (choice == 1) {
+		getline(cin, classname);
+		bool t = true;
+		Class* tmpClass = pClass;
+		while (tmpClass != nullptr)
 		{
-			in.ignore(100, ',');
-			if (in.eof())
-				break;
-			int ID;
-			in >> ID;
-			if (!check_id(ID, pStudent1))
+			if (tmpClass->name == classname)
+				t = false;
+			tmpClass = tmpClass->pNext;
+		}
+		deleteClassList(pClass);
+		if (t == false)
+		{
+			goToXY(x, y++); cout << "The class is existed." << endl;
+			goToXY(x, y++); cout << "Do you want to continue import another class? 1 for yes, 0 for no: ";
+			string yourchoice;
+			getline(cin, yourchoice);
+			bool check = convertStringToInt(yourchoice, choice);
+			while (!check || (choice != 1 && choice != 0))
 			{
-				dem++;
-				in.ignore();
-				string lastname;
-				getline(in, lastname, ',');
-				string firstname;
-				getline(in, firstname, ',');
-				int day;
-				in >> day;
-				in.ignore();
-				int month;
-				in >> month;
-				in.ignore();
-				int year;
-				in >> year;
-				in.ignore();
+				goToXY(x, y++); cout << "Please enter 0 or 1: ";
+				getline(cin, yourchoice);
+				check = convertStringToInt(yourchoice, choice);
 			}
+			if (choice == 0) return;
 			else
 			{
-				if (pStudent == nullptr)
-				{
-					pStudent = new Student;
-					pStudent->pNext = nullptr;
-					cur = pStudent;
-					success = 1;
-					dem ++;
-					Student* tmp = pStudent1;
-					if (pStudent1 == nullptr)
-					{
-						pStudent1 = pStudent;
-					}
-					else {
-						while (tmp->pNext != nullptr)
-							tmp = tmp->pNext;
-						tmp->pNext = pStudent;
-					}
-					numstudent1++;
-					cur->id = ID;
-				}
-				else
-				{
-					cur->pNext = new Student;
-					cur = cur->pNext;
-					cur->pNext = nullptr;
-					success++;
-					dem++;
-					numstudent1++;
-					cur->id = ID;
-				}
-				in.ignore(100, ',');
-				string lastname;
-				getline(in, lastname, ',');
-				string firstname;
-				getline(in, firstname, ',');
-				cur->fullname = lastname + " " + firstname;
-				in >> cur->dob.day;
-				in.ignore(100, '/');
-				in >> cur->dob.month;
-				in.ignore(100, '/');
-				in >> cur->dob.year;
-				cur->status = 1;
-				cur->cla = classname;
-				create_password(cur->dob, cur->password);
+				goToXY(x, y++); cout << "Enter class: ";
 			}
-		}
-		in.close();
-		out.close();
-		rewriteStudent(success, pStudent, classname);
-		rewriteStudent(numstudent1, pStudent1, "Student");
-		//update class.txt
-		int numClass1 = 0;
-		Class* pClass1 = nullptr;
-		loadClass(numClass1, pClass1);
-		Class* temp1 = pClass1;
-		numClass1++;
-		if (pClass1 == nullptr)
-		{
-			pClass1 = new Class;
-			pClass1->name = classname;
-			pClass1->pNext = nullptr;
 		}
 		else
 		{
-			Class* temp = pClass1;
-			while (temp->pNext != nullptr)
-				temp = temp->pNext;
-			temp->pNext = new Class;
-			temp = temp->pNext;
-			temp->pNext = nullptr;
-			temp->name = classname;
+			//chua cap nhat Class.txt o day vi con dem so hoc sinh
+			goToXY(x, y++); cout << "This is a new class." << endl;
+			system("pause");
+			break;
 		}
-		rewriteClass(numClass1, pClass1);
-		//
-		cout << "We have import " << success << " students successfully among " << dem << " students." << endl;
-		if (dem > success)
+	}
+	//end of checking
+	//import
+	if (choice == 0) return;
+	system("cls");
+	x = 10; y = 5;
+	goToXY(x, y++); cout << "You choose class " << classname << ".";
+	goToXY(x, y++); cout << "Please enter the link of csv file:";
+	string csv;
+	getline(cin, csv);
+	ifstream in;
+	in.open(csv);
+	while (!in.is_open())
+	{
+		goToXY(x, y++); cout << "Error when opening the csv file." << endl;
+		goToXY(x, y++); cout << "Do you want to:";
+		goToXY(x, y++); cout << "0.Exit.";
+		goToXY(x, y++); cout << "1. Enter another link.";
+		goToXY(x, y++); cout << "Your choice: ";
+		string yourChoice;
+		getline(cin, yourChoice);
+		bool check = convertStringToInt(yourChoice, choice);
+		while (!check || (choice != 0 && choice != 1))
 		{
-			cout << dem - success << " students failed because of existing id." << endl;
+			goToXY(x, y++); cout << "Please enter 0 or 1: ";
+			getline(cin, yourChoice);
+			check = convertStringToInt(yourChoice, choice);
 		}
-		deleteClassList(pClass1);
-		deleteStudentList(pStudent1);
+		if (choice == 0) return;
+		goToXY(x, y++); cout << "Please enter csv link: ";
+		getline(cin, csv);
+		in.open(csv);
+	}
+	string filename = classname + ".txt";
+	ofstream out;
+	out.open(filename);
+	if (!out.is_open())
+	{
+		goToXY(x, y++);  cout << "Error in creating the file." << endl;
+		return;
+	}
+	out << 0;
+	out.close();
+	int success = 0;
+	Student* pStudent = nullptr;
+	Student* cur = pStudent;
+	int dem = 0;
+	int numstudent1 = 0;
+	Student* pStudent1 = nullptr;
+	loadStudent(numstudent1, pStudent1, "Student");
+	in.ignore(1000, '\n');
+	while (!in.eof())
+	{
+		in.ignore(100, ',');
+		if (in.eof())
+			break;
+		int ID;
+		in >> ID;
+		if (!check_id(ID, pStudent1))
+		{
+			cout << "haha" << endl;
+			dem++;
+			in.ignore();
+			string lastname;
+			getline(in, lastname, ',');
+			string firstname;
+			getline(in, firstname, ',');
+			int day;
+			in >> day;
+			in.ignore();
+			int month;
+			in >> month;
+			in.ignore();
+			int year;
+			in >> year;
+			in.ignore();
+		}
+		else
+		{
+			if (pStudent == nullptr)
+			{
+				pStudent = new Student;
+				pStudent->pNext = nullptr;
+				cur = pStudent;
+				success = 1;
+				dem++;
+				Student* tmp = pStudent1;
+				if (pStudent1 == nullptr)
+				{
+					pStudent1 = pStudent;
+				}
+				else {
+					while (tmp->pNext != nullptr)
+						tmp = tmp->pNext;
+					tmp->pNext = pStudent;
+				}
+				numstudent1++;
+				cur->id = ID;
+			}
+			else
+			{
+				cur->pNext = new Student;
+				cur = cur->pNext;
+				cur->pNext = nullptr;
+				success++;
+				dem++;
+				numstudent1++;
+				cur->id = ID;
+			}
+			in.ignore(100, ',');
+			string lastname;
+			getline(in, lastname, ',');
+			string firstname;
+			getline(in, firstname, ',');
+			cur->fullname = lastname + " " + firstname;
+			in >> cur->dob.year;
+			in.ignore();
+			in >> cur->dob.month;
+			in.ignore();
+			in >> cur->dob.day;
+			in.ignore();
+			cur->status = 1;
+			cur->cla = classname;
+			create_password(cur->dob, cur->password);
+		}
+	}
+	in.close();
+	rewriteStudent(success, pStudent, classname);
+	rewriteStudent(numstudent1, pStudent1, "Student");
+	//update class.txt
+	int numClass1 = 0;
+	Class* pClass1 = nullptr;
+	loadClass(numClass1, pClass1);
+	Class* temp1 = pClass1;
+	numClass1++;
+	if (pClass1 == nullptr)
+	{
+		pClass1 = new Class;
+		pClass1->name = classname;
+		pClass1->pNext = nullptr;
+	}
+	else
+	{
+		Class* temp = pClass1;
+		while (temp->pNext != nullptr)
+			temp = temp->pNext;
+		temp->pNext = new Class;
+		temp = temp->pNext;
+		temp->pNext = nullptr;
+		temp->name = classname;
+	}
+	rewriteClass(numClass1, pClass1);
+	//
+	goToXY(x, y++); cout << "We have import " << success << " students successfully among " << dem << " students." << endl;
+	if (dem > success)
+	{
+		goToXY(x, y++); cout << dem - success << " students failed because of existing id." << endl;
+	}
+	deleteClassList(pClass1);
+	deleteStudentList(pStudent1);
 }
 
