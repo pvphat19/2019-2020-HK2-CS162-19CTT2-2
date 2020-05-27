5535ddd6a2330aa6404eca232b53d97ae0a8c988
pvphat19 <64125852+pvphat19@users.noreply.github.com>
2020-05-27 10:44:18 +0700

    hoan thien manually add student

--- FinalProject/FinalProject/add_import_students.cpp
+++ FinalProject/FinalProject/add_import_students.cpp
@@ -37,42 +37,89 @@ bool check_id(int ID, Student* pStudent)
 	return t;
 }
 
-void manually_add_student_to_class()
+bool checkValidDate(Date day)
 {
+	if (day.month == 1 || day.month == 3 || day.month == 5 || day.month == 7 || day.month == 8 || day.month || 10 || day.month == 12)
+	{
+		if (day.day > 0 && day.day <= 31)
+			return true;
+		else return false;
+	}
+	else if (day.month == 4 || day.month == 6 || day.month == 9 || day.month == 11)
+	{
+		if (day.day > 0 && day.day <= 30)
+			return true;
+		else return false;
+	}
+	if (day.day > 0 && day.day <= 28) return true;
+	else if (day.day == 29)
+	{
+		if (day.year % 4 != 0 || (day.year % 100 == 0 && day.year % 400 != 0))
+			return false;
+		return true;
+	}
+	return false;
+}
+
+void manually_add_student_to_class()//chua kt ngay thang nam
+{
+	int x;
+	int y;
+	system("cls");
+	x = 10; y = 5;
 	//input class
-	int numClass=0;
-	Class* pClass=nullptr;
+	int numClass = 0;
+	Class* pClass = nullptr;
 	loadClass(numClass, pClass);
-	Class* cur=pClass;
-	cout << "Please choose the class you want to add in. We have " << numClass << " class:" << endl;
+	Class* cur = pClass;
+	goToXY(x, y++); cout << "Manually add a student to a class.";
+	goToXY(x, y++); cout << "Please choose the class you want to add in. We have " << numClass << " class:";
 	for (int i = 0; i < numClass; ++i)
 	{
-		cout << i + 1 << "." << cur->name << endl;
+		goToXY(x, y++); cout << i + 1 << "." << cur->name << endl;
 		cur = cur->pNext;
 	}
-	cout << "Enter the class you want: ";
-	int n;
-	cin >> n;
+	goToXY(x, y++); cout << "Enter the order of class you want: ";
+	string tmpchoice;
+	int n = 0;
+	getline(cin, tmpchoice);
+	bool check = convertStringToInt(tmpchoice, n);
+	while (!check || (n <= 0 || n > numClass))
+	{
+		goToXY(x, y++); cout << "Please enter a number from 1 to " << numClass << ": ";
+		getline(cin, tmpchoice);
+		check = convertStringToInt(tmpchoice, n);
+	}
 	cur = pClass;
 	for (int i = 0; i < n - 1; ++i)
 		cur = cur->pNext;
-	cout << "You choose class " << cur->name<<"." << endl;
+	system("cls"); x = 10; y = 5;
+	goToXY(x, y++); cout << "You choose class " << cur->name << "." << endl;
 	//check id
-	Student* pStudent1=nullptr;
-	int numStudent1=0;
-	loadStudent(numStudent1, pStudent1, "Student");
-	cout << "Please enter student ID: ";
+	Student* pAllStudent = nullptr;
+	int numAllStudent = 0;
+	loadStudent(numAllStudent, pAllStudent, "Student");
+	goToXY(x, y++); cout << "Please enter student ID: ";
+	string tmpID;
+	getline(cin, tmpID);
 	int ID;
-	cin >> ID;
-	while (check_id(ID,pStudent1) == false) {
-		cout << "The ID has been used. Please enter new student ID: ";
-		cin >> ID;
+	check = convertStringToInt(tmpID, ID);
+	while (!check || check_id(ID, pAllStudent) == false) {
+		if (!check) {
+			goToXY(x, y++); cout << "Please enter a number: ";
+		}
+		else
+		{
+			goToXY(x, y++); cout << "The ID has been used. Please enter another ID: ";
+		}
+		getline(cin, tmpID);
+		check = convertStringToInt(tmpID, ID);
 	}
-	//add to the class file
-	Student* pStudent=nullptr;
-	int numStudent=0;
+	//cap nhat file cua lop
+	Student* pStudent = nullptr;
+	int numStudent = 0;
 	loadStudent(numStudent, pStudent, cur->name);
-	Student * tmp = pStudent;
+	Student* tmp = pStudent;
 	if (tmp == nullptr)
 	{
 		pStudent = new Student;
@@ -88,28 +135,84 @@ void manually_add_student_to_class()
 	tmp->id = ID;
 	tmp->cla = cur->name;
 	tmp->status = 1;
-	cout << "Please enter full name of the student: ";
-	cin.ignore(100, '\n');
-	getline(cin,tmp->fullname);
-	cout << "Enter date of birth: " << endl;
-	cout << "Day: ";
-	cin >> tmp->dob.day;
-	cout << "Month: ";
-	cin >> tmp->dob.month;
-	cout << "Year: ";
-	cin >> tmp->dob.year;
+	goToXY(x, y++); cout << "Please enter full name of the student: ";
+	getline(cin, tmp->fullname);
+	goToXY(x, y++); cout << "Enter date of birth: " << endl;
+	goToXY(x, y++); cout << "Day: ";
+	string dobDay;
+	getline(cin, dobDay);
+	check = convertStringToInt(dobDay, tmp->dob.day);
+	while (!check && (tmp->dob.day <= 0 || tmp->dob.day > 31))
+	{
+		goToXY(x, y++); cout << "Please enter a positive integer: ";
+		getline(cin, dobDay);
+		check = convertStringToInt(dobDay, tmp->dob.day);
+	}
+	goToXY(x, y++); cout << "Month: ";
+	string dobMonth;
+	getline(cin, dobMonth);
+	check = convertStringToInt(dobMonth, tmp->dob.month);
+	while (!check && (tmp->dob.month <= 0 || tmp->dob.month > 31))
+	{
+		goToXY(x, y++); cout << "Please enter a positive integer: ";
+		getline(cin, dobMonth);
+		check = convertStringToInt(dobMonth, tmp->dob.month);
+	}
+	goToXY(x, y++); cout << "Year: ";
+	string dobYear;
+	getline(cin, dobYear);
+	check = convertStringToInt(dobYear, tmp->dob.year);
+	while (!check && (tmp->dob.year <= 0 || tmp->dob.year > 31))
+	{
+		goToXY(x, y++); cout << "Please enter a positive integer: ";
+		getline(cin, dobYear);
+		check = convertStringToInt(dobYear, tmp->dob.year);
+	}
+	while (!checkValidDate(tmp->dob))
+	{
+		goToXY(x, y++); cout << "The date is not existed. Please enter another date: ";
+		goToXY(x, y++); cout << "Day: ";
+		string dobDay;
+		getline(cin, dobDay);
+		check = convertStringToInt(dobDay, tmp->dob.day);
+		while (!check && (tmp->dob.day <= 0 || tmp->dob.day > 31))
+		{
+			goToXY(x, y++); cout << "Please enter a positive integer: ";
+			getline(cin, dobDay);
+			check = convertStringToInt(dobDay, tmp->dob.day);
+		}
+		goToXY(x, y++); cout << "Month: ";
+		string dobMonth;
+		getline(cin, dobMonth);
+		check = convertStringToInt(dobMonth, tmp->dob.month);
+		while (!check && (tmp->dob.month <= 0 || tmp->dob.month > 31))
+		{
+			goToXY(x, y++); cout << "Please enter a positive integer: ";
+			getline(cin, dobMonth);
+			check = convertStringToInt(dobMonth, tmp->dob.month);
+		}
+		goToXY(x, y++); cout << "Year: ";
+		string dobYear;
+		getline(cin, dobYear);
+		check = convertStringToInt(dobYear, tmp->dob.year);
+		while (!check && (tmp->dob.year <= 0 || tmp->dob.year > 31))
+		{
+			goToXY(x, y++); cout << "Please enter a positive integer: ";
+			getline(cin, dobYear);
+			check = convertStringToInt(dobYear, tmp->dob.year);
+		}
+	}
 	create_password(tmp->dob, tmp->password);
 	numStudent++;
-	rewriteStudent(numStudent, pStudent, cur->name);
-	//add to student.txt
-	Student *tmp1 = pStudent1;
+	//cap nhat student.txt
+	Student* tmp1 = pAllStudent;
 	if (tmp1 == nullptr)
 	{
-		pStudent1 = new Student;
-		tmp1 = pStudent1;
+		pAllStudent = new Student;
+		tmp1 = pAllStudent;
 	}
 	else {
-		for (int i = 0; i < numStudent1 - 1; ++i)
+		for (int i = 0; i < numAllStudent - 1; ++i)
 			tmp1 = tmp1->pNext;
 		tmp1->pNext = new Student;
 		tmp1 = tmp1->pNext;
@@ -121,12 +224,29 @@ void manually_add_student_to_class()
 	tmp1->fullname = tmp->fullname;
 	tmp1->password = tmp->password;
 	tmp1->status = tmp->status;
-	numStudent1++;
-	rewriteStudent(numStudent1,pStudent1,"Student");
+	numAllStudent++;
+	goToXY(x, y++); cout << "Do you want to add this student to class " << cur->name << "? 1 to confirm, 0 for exist: ";
+	getline(cin, tmpchoice);
+	int choice;
+	check = convertStringToInt(tmpchoice, choice);
+	while (!check || (choice != 0 && choice != 1))
+	{
+		goToXY(x, y++); cout << "Please enter 0 or 1: ";
+		getline(cin, tmpchoice);
+		check = convertStringToInt(tmpchoice, choice);
+	}
+	if (choice == 1) {
+		rewriteStudent(numStudent, pStudent, cur->name);
+		rewriteStudent(numAllStudent, pAllStudent, "Student");
+		goToXY(x, y++); cout << "The student has been added to system." << endl;
+	}
+	else
+	{
+		goToXY(x, y++); cout << "The student has not been added to system" << endl;
+	}
 	deleteStudentList(pStudent);
 	deleteClassList(pClass);
-	deleteStudentList(pStudent1);
-	cout << "The student has been add to symstem." << endl;
+	deleteStudentList(pAllStudent);
 }
 
 void import_class()//can nang cap doan check class ban dau
@@ -308,3 +428,4 @@ void import_class()//can nang cap doan check class ban dau
 		deleteClassList(pClass1);
 		deleteStudentList(pStudent1);
 }
+
